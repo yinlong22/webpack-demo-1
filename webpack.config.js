de0279542664ad549f2fe755ad4a15c7d3557964
yinlong22 <342241857@.com>
2020-04-17 22:22:52 +0800

    updata

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js'
-    }
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: "Webpack App",
+    })],
+    module: {
+        rules: [
+            {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
