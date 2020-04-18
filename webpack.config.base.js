2145e99e40b0a3904327d320c6ef00b9c0c4924a
yinlong22 <342241857@.com>
2020-04-18 11:22:20 +0800

    updata

---
+++ webpack.config.base.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack App",
+            template: "src/index.html"
+        }),
+    ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+const base = require('./webpack.config.base')
+
 module.exports = {
-    mode: 'development',
+    ...base,
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
     },
-    entry: './src/index.js',
-    output: {
-        filename: 'index.[contenthash].js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack App",
-            template: "src/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash]. css',
-            ignoreOrder: false,
-        }),
-    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../',
-                            hrm: process.env.NODE_ENV === 'development',
-                        },
-                    },
-                    'css-loader',
-                ],
-                // use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader']
             }
         ]
 
